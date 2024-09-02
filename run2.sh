af0d8b50a4a4dac0b2467358060a27bcc280bf84
asher gray <asher@themakerspace.co.za>
2024-09-02 11:01:29 +0200

    different env for other method

--- run2.sh
+++ run2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Create and activate a virtual environment
-python3 -m venv audio_monitor_env
-source audio_monitor_env/bin/activate
+python3 -m venv video_loop_env
+source video_loop_env/bin/activate
 
 # Install required packages
 pip install -r requirements2.txt
